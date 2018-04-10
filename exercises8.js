6ee04cf5aeeab5169c0db2976afb2c7dc81003c9
shantidyah <shanti.dyah@gmail.com>
2018-04-11 00:28:30 +0700

    Urutkan abjad

---
+++ exercises8.js
@@ -0,0 +1,11 @@
+function urutkanAbjad(str) {
+  var balik=str.split('').sort().join('');
+  return balik;
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
