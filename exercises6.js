62d12dc30d4116aeadd8b4ea4b8176a79c85e4ac
shantidyah <shanti.dyah@gmail.com>
2018-04-11 00:27:57 +0700

    hitung huruf

---
+++ exercises6.js
@@ -0,0 +1,33 @@
+function hitungHuruf(kata) {
+  var jum=[];
+  var hasil=0;
+  var pjgkata=[];
+
+  kata=kata.split(' ');
+
+  for(i=0;i<kata.length;i++){
+    var b='';
+    for(j=0;j<kata[i].length;j++){
+      if(b.indexOf(kata[i].charAt(j))==-1){
+        b+=kata[i][j];
+      }
+    }
+    jum.push(b);
+  }
+  for(k=0;k<kata.length;k++){
+    hasil=kata[k].length-jum[k].length;
+    pjgkata.push(hasil);
+  }
+  var a=Math.max.apply(null,pjgkata);
+  pjgkata=pjgkata.indexOf(a);
+
+
+console.log(kata[pjgkata]);
+}
+
+// TEST CASES
+console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
+console.log(hitungHuruf('I am a passionate developer')); // passionate
+console.log(hitungHuruf('aku adalah anak gembala')); // adalah
+console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
+console.log(hitungHuruf('mengayuh perahu di danau')); // danau
